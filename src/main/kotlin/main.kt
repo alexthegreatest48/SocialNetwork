55072d719162cc3c22ba06158a7588fa309af8e3
alexthegreatest48 <as.anisimov@inbox.ru>
2022-11-30 22:32:35 +0300

    New classes for attachments added

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -37,6 +37,42 @@ data class PhotoAttachment(val photo: Photo): Attachment{
     override val type: String = "photo"
 }
 
+data class Doc(
+    val id: Int,
+    val format: String,
+    val ownerId: Int,
+    val userId: Int,
+    val title: String,
+)
+
+data class Audio(
+    val id: Int,
+    val duration: Int,
+    val ownerId: Int,
+    val userId: Int,
+    val title: String,
+)
+
+data class Link(
+    val id: Int,
+    val internal: Boolean,
+    val ownerId: Int,
+    val userId: Int,
+    val title: String,
+)
+
+data class DocAttachment(val doc: Doc): Attachment{
+    override val type: String = "doc"
+}
+
+data class AudioAttachment(val audio: Audio): Attachment{
+    override val type: String = "audio"
+}
+
+data class LinkAttachment(val link: Link): Attachment{
+    override val type: String = "link"
+}
+
 object WallService{
     private var posts = emptyArray<Post>()
     private var counter = 1;
@@ -69,9 +105,11 @@ object WallService{
 }
 
 fun main(){
+    val audio1: Audio = Audio(1,234,24,23,"New Album")
     val video1: Video = Video(1,23,1, 24, "Best cats video 2022")
     val attach1 : VideoAttachment = VideoAttachment(video1)
-    WallService.add(Post(1, 23, attachments =  arrayOf(attach1)))
+    val attach2 : AudioAttachment = AudioAttachment(audio1)
+    WallService.add(Post(1, 23, attachments =  arrayOf(attach1,attach2)))
     WallService.add(Post(1, 24, "Text"))
     WallService.printAll()
     println()
